c1cf6ea94df361f859f27e66e6419965ad3090ee
kabuda-czh <634469564@qq.com>
2022-05-16 14:42:21 +0800

    增加vuetify配置, vite配置, pinia配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,25 @@ import { defineNuxtConfig } from 'nuxt'
 export default defineNuxtConfig({
   // 需要安装 TypeScript Vue Plugin (Volar) 在Vscode中, 如果没有则下面的设置不需要
   typescript: {
-    shim: false
+    shim: false,
+    strict: true,
   },
+  css: ["vuetify/lib/styles/main.sass"],
+  buildModules: [
+    // pinia plugin - https://pinia.esm.dev
+    "@pinia/nuxt",
+  ],
+  vite: {
+    logLevel: "info",
+    optimizeDeps: {
+      include: ["vue", "pinia"],
+    },
+    define: {
+      "process.env.DEBUG": "false",
+    },
+  },
+  build: {
+    transpile: ["vuetify"],
+  },
+  meta: {},
 })
