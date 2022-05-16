74bb4b253aea7de2ca70c22e839ce389f82d0bff
kabuda-czh <634469564@qq.com>
2022-05-16 14:41:56 +0800

    增加plugin, 添加vuetify以及mdi

---
+++ plugins/index.ts
@@ -0,0 +1,22 @@
+import { defineNuxtPlugin } from "#app";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import { aliases, mdi } from "vuetify/lib/iconsets/mdi";
+import "@mdi/font/css/materialdesignicons.css";
+
+export default defineNuxtPlugin(async (nuxtApp) => {
+	const vuetify = createVuetify({
+		components,
+		directives,
+		icons: {
+			defaultSet: "mdi",
+			aliases,
+			sets: {
+				mdi,
+			},
+		},
+	});
+
+	nuxtApp.vueApp.use(vuetify);
+});
