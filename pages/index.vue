0c5bcda6c72e2679e24d79d8b035dc362e04a9fb
kabuda-czh <634469564@qq.com>
2022-05-16 17:30:10 +0800

    修改"/"路由分布

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,15 @@
 <template>
   <div>
-    <v-card class="mx-auto" max-width="300">
-      <v-list disabled>
-        <v-list-subheader>REPORTS</v-list-subheader>
-        <v-list-item v-for="(item, i) in items" :key="i">
-          <v-list-item-avatar start>
-            <v-icon :icon="item.icon"></v-icon>
-          </v-list-item-avatar>
-          <v-list-item-title v-text="item.text"></v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-card>
+    <NuxtLayout name="home">
+      <template #drawer>
+        <Drawer />
+      </template>
+      <template #appbar>
+        <AppBar />
+      </template>
+      <NuxtChild />
+    </NuxtLayout>
   </div>
 </template>
 
-<script setup lang="ts" >
-const selectedItem = ref(1);
-const items = ref([
-  { text: "Real-Time", icon: "mdi-clock" },
-  { text: "Audience", icon: "mdi-account" },
-  { text: "Conversions", icon: "mdi-flag" },
-]);
-</script>
+<script setup lang="ts"></script>
