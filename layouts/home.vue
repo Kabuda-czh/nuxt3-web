fdf3291831c6dc7829acd2e5a60c5e0dfceda3b7
kabuda-czh <634469564@qq.com>
2022-05-16 17:29:47 +0800

    总体布局

---
+++ layouts/home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <v-app>
+      <v-navigation-drawer app>
+        <slot name="drawer" />
+      </v-navigation-drawer>
+      <v-app-bar app>
+        <slot name="appbar" />
+      </v-app-bar>
+      <v-main>
+        <v-container fluid>
+          <slot />
+        </v-container>
+      </v-main>
+      <v-footer app>
+        <!--  -->
+      </v-footer>
+    </v-app>
+  </div>
+</template>
