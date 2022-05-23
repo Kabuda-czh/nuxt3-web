cb68578396be3487855b0646bd8874ea06440c1f
kabuda-czh <634469564@qq.com>
2022-05-23 17:33:09 +0800

    增加blogs的子页面

---
+++ pages/blogs/[id].vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="BlogById">
+    <div class="BlogByIdLeft"></div>
+    <div class="BlogByIdContent"></div>
+    <div class="BlogByIdRight"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { findById } from "~~/service/blogs";
+
+const route = useRoute();
+
+const { data } = await useAsyncData(
+  "blogid",
+  async () => await findById(String(route.params.id))
+);
+
+console.log(data)
+</script>
+
+<style lang="scss" scoped>
+.BlogById {
+  width: 80%;
+  min-height: 200px;
+  margin: 20px auto;
+  display: flex;
+  gap: 30px;
+  .BlogByIdLeft {
+    flex: 0 0 20%;
+    border: 1px #ccc solid;
+  }
+  .BlogByIdContent {
+    flex: 0 0 60%;
+    border: 1px #ccc solid;
+  }
+  .BlogByIdRight {
+    flex: 0 0 20%;
+    border: 1px #ccc solid;
+  }
+}
+</style>
